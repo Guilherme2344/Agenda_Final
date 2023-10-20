abe0585d84c7a4b2e01336c18170e9878f86a475
Guilherme Cristiano <124366188+Guilherme2344@users.noreply.github.com>
2023-10-20 11:32:09 -0300

    Update manterclienteUI.py

--- manterclienteUI.py
+++ manterclienteUI.py
@@ -3,7 +3,7 @@ from view import View
 
 class ManterClienteUI:
     def main():
-        st.header('Sistema de Agenda')
+        st.header('Sistema de Cliente')
         tab_1, tab_2, tab_3, tab_4 = st.tabs(['Listar', 'Inserir', 'Atualizar', 'Excluir'])
         with tab_1: View.cliente_listar()
         with tab_2: View.cliente_inserir()
